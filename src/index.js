924f99732edba56fbf5a4f90f8de59ea44e88a26
Jose Salazar <josdsalzarmor@unal.edu.co>
2019-05-12 01:31:59 -0500

    asasd

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-<<<<<<< Updated upstream
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-=======
+
 class Board extends React.Component {
     renderSquare(i) {
         return (
@@ -164,4 +163,3 @@ function calculateWinner(squares) {
     }
     return null;
 }
->>>>>>> Stashed changes
